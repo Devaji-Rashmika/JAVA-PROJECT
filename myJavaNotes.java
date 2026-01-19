9bea9aa74a8f8ab67e566f9206e29ff89cd09820
Devaji-Rashmika <devajirashmika@gmail.com>
2026-01-19 20:09:36 +0530

    Create myJavaNotes.java

---
+++ myJavaNotes.java
@@ -0,0 +1,32 @@
+// import java.util.*;
+// public class BitwiseShiftOperations {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     System.out.println("1.Bitwise left shift (n<<3)");
+//     System.out.println("2.Bitwise right shift (n>>3)");
+//     System.out.println("3.Bitwise unsigned Right shift(n>>>4)");
+//     System.out.println("Exit");
+//     int choice = sc.nextInt();
+//     switch (choice) {
+//         case 1:
+//             System.out.println((n << 3));
+//             break;
+//         case 2:
+//             System.out.println((n >> 3));
+//             break;
+//         case 3:
+//             System.out.println( (n >>> 4));
+//             break;
+//         case 4:
+//             System.out.println("Exit");
+//             break;
+//     }}}
+import java.util.*;
+public class myJavaNotes {
+    public static void printMyName(String name){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        printMyName(name);
+    }
+}
